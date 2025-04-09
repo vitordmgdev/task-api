f18e244b29827e46ba1021558908262447e3b0b1
vitor.domingues.dev <123842676+vitordmgdev@users.noreply.github.com>
2025-04-09 10:25:42 -0300

    feat: login into controller and changes in register

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,31 +1,63 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
-import { RegisterInput } from '../schemas/auth-schema';
+import { loginSchema, registerSchema } from '../schemas/auth-schema';
 import AuthService from '../services/auth.services';
 
-type RegisterBody = z.infer<typeof RegisterInput>;
+export type RegisterBody = z.infer<typeof registerSchema>;
+export type LoginBody = z.infer<typeof loginSchema>;
 
 const AuthController = {
     register: async (
         request: FastifyRequest<{ Body: RegisterBody }>,
         reply: FastifyReply,
     ): Promise<void> => {
-        const parse = RegisterInput.safeParse(request.body);
+        const parse = await registerSchema.safeParseAsync(request.body);
+        const userRequestBody = request.body;
 
         if (!parse.success) {
             return reply.code(400).send({
-                message: 'Error de validação',
+                message: 'Dados inválidos.',
                 errors: parse.error.flatten().fieldErrors,
             });
-        };
+        }
 
-        const userRequestBody = request.body;
-        const user = await AuthService.createUser(userRequestBody);        
+        const user = await AuthService.createUser(userRequestBody);
+
+        if (!user) {
+            return reply.code(400).send({ message: 'Erro ao cadastrar usuário!'});
+        }
 
         return reply.code(201).send({
-            message: 'Usuário criado com sucesso',
-            user,
+            message: 'Conta criada com sucesso!',
         });
+        
+    },
+    login: async (
+        request: FastifyRequest<{ Body: LoginBody }>,
+        reply: FastifyReply,
+    ): Promise<void> => {
+        const user = request.body;
+
+        const parse = await loginSchema.safeParseAsync(request.body);
+
+        if (!parse.success) {
+            return reply.code(400).send({
+                message: 'Dados inválidos!',
+                errors: parse.error.flatten().fieldErrors,
+            });
+        }
+
+        const token = await AuthService.login(user.email, user.password);
+
+        if (!token) {
+            return reply.code(400).send(
+                { message: 'Houve erro ao retornar ao tentar fazer login!' }
+            );
+        }
+
+        return reply.code(200).send(
+            { message: 'Login feito com sucesso!', token: token }
+        );
     },
 };
 
